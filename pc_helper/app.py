a1ca9632dbe62a79d2b8ec44c3806298afb1ad9d
kusisgora-ux <kusisgora@gmail.com>
2026-05-13 23:07:42 +0300

    Add files via upload

--- pc_helper/app.py
+++ pc_helper/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import sqlite3
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates", static_folder="static")
 show_smart = False
 
 # =========================
